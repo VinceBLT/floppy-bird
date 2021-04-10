a7ba356736b8f8c04de73fdf0f759dbf644ce3eb
Vincent <vincent.bezault@icloud.com>
2021-04-10 16:28:24 +0200

    moving background + placing bird

---
+++ script.js
@@ -0,0 +1,61 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const img = new Image();
+img.src = "./media/flappy-bird-set.png";
+
+// General settings
+let gamePlaying = false;
+const gravity = 0.5;
+const speed = 6.2;
+const size = [51, 36];
+const jump = -11.5;
+const cTenth = canvas.width / 10;
+
+let index = 0,
+   bestScore = 0,
+   currentScore = 0,
+   pipes = [],
+   flight,
+   flyHeight;
+flyHeight = canvas.height / 2 - size[1] / 2;
+
+const render = () => {
+   index++;
+   // Bird
+   ctx.drawImage(
+      img,
+      432,
+      Math.floor((index % 9) / 3) * size[1],
+      ...size,
+      canvas.width / 2 - size[0] / 2,
+      flyHeight,
+      ...size
+   );
+   // Background
+   ctx.drawImage(
+      img,
+      0,
+      0,
+      canvas.width,
+      canvas.height,
+      -((index * (speed / 2)) % canvas.width) + canvas.width,
+      0,
+      canvas.width,
+      canvas.height
+   );
+   // 2nd Background
+   ctx.drawImage(
+      img,
+      0,
+      0,
+      canvas.width,
+      canvas.height,
+      -((index * (speed / 2)) % canvas.width),
+      0,
+      canvas.width,
+      canvas.height
+   );
+
+   window.requestAnimationFrame(render);
+};
+img.onload = render;
